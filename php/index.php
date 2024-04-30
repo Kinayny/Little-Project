55808564814207998ec30ae9825acd8053a2b3eb
Kinayny <165022980+Kinayny@users.noreply.github.com>
2024-04-30 18:01:38 +0200

    Variables

--- php/index.php
+++ php/index.php
@@ -2,10 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PhP</title>
+    <title>PHP</title>
 </head>
 <body>
     <h1>Hey Local</h1>
+
+    <?php
+    $age = 25; // déclaration d'une variable "age" avec la valeur "25"
+    $prenom = "Jean"; // déclaration d'une variable "prenom" avec la valeur "Jean"
+    $solde = 120.50; // déclaration d'une variable "solde" avec la valeur "120.50"
+    $message = "Bonjour, ".$prenom."!"; // création d'une chaîne avec la concaténation de variables
+    echo $message; // affichage de la chaîne
+    ?>
+
 </body>
 </html>
