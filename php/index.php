51bb24cdefe3c1ac6d33318ac11f7e0f2fb0c4b1
Kinayny <165022980+Kinayny@users.noreply.github.com>
2024-05-01 15:17:19 +0200

    Variable

--- php/index.php
+++ php/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
 </head>
+
 <body>
     <h1>Hey Local</h1>
 
@@ -13,9 +15,22 @@
     $age = 25; // déclaration d'une variable "age" avec la valeur "25"
     $prenom = "Jean"; // déclaration d'une variable "prenom" avec la valeur "Jean"
     $solde = 120.50; // déclaration d'une variable "solde" avec la valeur "120.50"
-    $message = "Bonjour, ".$prenom."!"; // création d'une chaîne avec la concaténation de variables
+    $message = "Bonjour, " . $prenom . "!"; // création d'une chaîne avec la concaténation de variables
     echo $message; // affichage de la chaîne
+
+    $name = 'Ekko';
+    $firstname = 'Xin';
+    $note1 = 10;
+    $note2 = 20;
+    $moyenne = (($note1 + $note2) / 2);
+
+    echo "Bonjour " . $name . ' ' . $firstname . " vous avez eu " . (($note1 + $note2) / 2);
+
+    echo "<br> Bonjour $name $firstname, vous avez eu $moyenne de moyenne.";
     ?>
 
+
+
 </body>
+
 </html>
