3fd809eb99324583a611b351249a193a69444a6c
Kinayny <165022980+Kinayny@users.noreply.github.com>
2024-05-04 12:52:14 +0200

    Method Post

---
+++ php/Fundamental/registration.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php registration</title>
+</head>
+<body>
+
+
+
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+
+echo "Bonjour: ".$name;
+echo " ton email est: ".$email;
+?>
+    
+</body>
+</html>
