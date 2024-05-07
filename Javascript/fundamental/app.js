90576e80451300d739315e4b868abb86a8e406bc
Kinayny <165022980+Kinayny@users.noreply.github.com>
2024-05-07 12:37:56 +0200

    condition ternaire

--- Javascript/fundamental/app.js
+++ Javascript/fundamental/app.js
@@ -1,1 +1,8 @@
-console.log("bonjour");
+let age = 15;
+console.log(`vous avez ${age} ans, vous etes donc ${(age>=18 )?"Majeur" : "Mineur"}`);
+
+
+let enForme = "sportif";
+console.log(`Vous êtes ${enForme}, vous êtes donc ${(enForme == "sportif") ? "Tres fort" : "Faites vos preuves"}`);
+
+
