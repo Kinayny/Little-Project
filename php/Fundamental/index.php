6b172112d3d849a3bb3598cadeef9962e3a7960d
Kinayny <165022980+Kinayny@users.noreply.github.com>
2024-05-17 11:26:25 +0200

    isset

--- php/Fundamental/index.php
+++ php/Fundamental/index.php
@@ -1,53 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PhP</title>
+    <title>Document</title>
 </head>
-
 <body>
+    
+    <form action="./registration.php" method="POST">
 
-
-<form action="registration.php" method="POST">
-    <label for="name">Name: </label>
-    <input type="text" id="name" name="name">
-    <br>
-    <br>
-    <label for="email">Email: </label>
-    <input type="email" id="email" name="email">
-    <br>
-    <br>
-    <input type="submit" value="send">
-</form>
+    <label for="nom">Nom</label>
+    <input type="text" name="nom" id="nom">
+    <label for="prenom">Prenom</label>
+    <input type="text" name="prenom" id="prenom">
+    <input type="submit" value="Login">
 
 
 
 
-    <?php
-    $name = 'Ekko';
-    $firstname = 'Xin';
-    $note1 = 10;
-    $note2 = 20;
-    $moyenne = (($note1 + $note2) / 2);
 
-    echo "<br> <br> <br> <br>Bonjour " . $name . ' ' . $firstname . " vous avez eu " . (($note1 + $note2) / 2);
-
-    echo "<br> Bonjour $name $firstname, vous avez eu $moyenne de moyenne. <br> <br>";
-
-    // $message = "Bonjour, {$name}! Tu as {$note2} de moyenne.";
-    $message = "Bonjour, $name! Tu as $note2 de moyennesss.<br> <br> <br> <br>";
-
-    echo $message;
-
-
-    ?>
-
-
-
-
+    </form>
+        
 
 </body>
-
 </html>
--- php/Fundamental/registration.php
+++ php/Fundamental/registration.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php registration</title>
-</head>
-<body>
+<?php 
 
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
 
+    if(isset($_POST['nom']) && ($_POST['prenom'])){
 
-<?php
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-
-
-echo "Hello: ".$name;
-echo " your email is: ".$email;
-?>
-    
-</body>
-</html>
+        echo 'Bonjour '.$nom . ' ' .$prenom;
+    };
