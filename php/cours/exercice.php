dbd6d65d9f3f9fb98a18aecd25fd1711801b2096
Kinayny <165022980+Kinayny@users.noreply.github.com>
2024-06-06 17:30:18 +0200

    php

--- php/cours/exercice.php
+++ php/cours/exercice.php
@@ -6,6 +6,50 @@
     <title>Document</title>
 </head>
 <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercices</title>
+</head>
+<body>
+
+    <h1 style="text-align: center;">Hello</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+        <!-- *******  PHP ***** -->
+    <?php 
+
+    $arme1 = "Hache";
+    $arme2 = "Epee";
+    $degatHache = 70;
+    $degatEpee = 30;
+
+    $arme = "Hache";
+
+
+    
+    if($arme === $arme1){
+            echo "Vous avez recus un coup de Hache de: ";
+        } elseif ($arme === $arme2){
+            echo "vous avez recus un petit coup d'epee";
+        } else {
+            echo "Vous devez equippé une arme";
+        }
+    ?>
+</body>
+</html>
     
 </body>
 </html>
